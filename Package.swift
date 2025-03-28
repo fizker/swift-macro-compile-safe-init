35af2fed85b553987bcbf087c3716111e83cc288
Benjamin Horsleben <benjamin@fizkerinc.dk>
2025-03-28 01:45:38 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,54 @@
+// swift-tools-version: 6.0
+
+import CompilerPluginSupport
+import PackageDescription
+
+let package = Package(
+	name: "swift-macro-compile-safe-init",
+	platforms: [
+		.iOS(.v13),
+		.macCatalyst(.v13),
+		.macOS(.v10_15),
+		.tvOS(.v13),
+		.watchOS(.v6),
+	],
+	products: [
+		.library(
+			name: "CompileSafeInitMacro",
+			targets: ["CompileSafeInitMacro"]
+		),
+	],
+	dependencies: [
+		.package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.1"),
+	],
+	targets: [
+		.target(
+			name: "CompileSafeInitMacro",
+			dependencies: [
+				"CompileSafeInitMacroImplementation",
+			]
+		),
+		.macro(
+			name: "CompileSafeInitMacroImplementation",
+			dependencies: [
+				.product(name: "SwiftSyntax", package: "swift-syntax"),
+				.product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+				.product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+			]
+		),
+	]
+)
+
+#if !os(Windows)
+// Note: This next comment is unverified, it was lifted from https://github.com/swiftlang/swift-syntax/blob/2da6a366aeb1d525bc654dec3e493acb60eeb4f3/Examples/Package.swift
+// We can't write a test target for macros on Windows because that results in duplicate definitoions of `main`: Once
+// from the macro (which is effectively an executable), and once from the test bundle.
+package.targets.append(.testTarget(
+	name: "CompileSafeInitMacroImplementationTests",
+	dependencies: [
+		"CompileSafeInitMacroImplementation",
+		.product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+		.product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
+	]
+))
+#endif
---
+++ Tests/CompileSafeInitMacroImplementationTests/swift_macro_compile_safe_initTests.swift
@@ -0,0 +1,6 @@
+import Testing
+@testable import swift_macro_compile_safe_init
+
+@Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
